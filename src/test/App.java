3a782efa7c49911d3015126bc275f494227e3a49
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-07-19 15:32:01 +0300

    first test

---
+++ src/test/App.java
@@ -0,0 +1,11 @@
+package test;
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(new Person().getFormHTML("traitement", "get"));
+
+    }
+
+
+}
+
