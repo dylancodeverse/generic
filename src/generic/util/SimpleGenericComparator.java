a88ad9f7b4fde1a3342248ba3ac64c77a598bae9
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-07-19 18:53:24 +0300

    SimpleGenericComparator

---
+++ src/generic/util/SimpleGenericComparator.java
@@ -0,0 +1,61 @@
+package generic.util;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SimpleGenericComparator implements Comparator<Object>
+{
+
+    String [] defaultFieldsReference ;
+
+
+    public SimpleGenericComparator(Object objectInstance, String[] defaultFieldsReference ) throws IllegalArgumentException 
+    {
+        String[] fieldsName = Reflect.getAttributeName(objectInstance);
+
+        // Vérifier que chaque élément de defaultFieldsReference est dans fieldsName
+        for (String field : defaultFieldsReference) 
+        {
+            if (!Arrays.asList(fieldsName).contains(field)) 
+            {
+                throw new IllegalArgumentException("Le champ référencé par defaultFieldsReference n'existe pas dans l'objet.");
+            }
+        }
+
+        // Si tous les champs de defaultFieldsReference sont valides, les assigner à l'attribut de classe
+        this.defaultFieldsReference = defaultFieldsReference;
+
+    }
+
+    
+    @Override
+    public int compare(Object o1, Object o2) 
+    {
+        int response =0;
+        for (int i = 0; i < defaultFieldsReference.length; i++) 
+        {
+            try 
+            {
+                Object o1Value = Reflect.getValue(o1, new String []{defaultFieldsReference[i]} ,0) ;
+                Object o2Value = Reflect.getValue(o2, new String []{defaultFieldsReference[i]},0);
+
+                response = Reflect.compare(o1Value, o2Value) ;
+                if (response!=0) 
+                {
+                    return response;
+                }           
+            } 
+            catch (Exception e) 
+            {
+                return response;
+            }
+ 
+        }
+        return response ;
+    }
+
+    public void setDefaultFieldsReference(String [] defaultFieldsReference) 
+    {
+        this.defaultFieldsReference = defaultFieldsReference;
+    }
+
+}
