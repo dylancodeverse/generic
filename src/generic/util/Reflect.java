547cddeb4c9993cedd8d9fffb4222553f80d94b4
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-07-19 18:03:23 +0300

    Nouvelle methode pour GenericComparator

--- src/generic/util/Reflect.java
+++ src/generic/util/Reflect.java
@@ -84,4 +84,36 @@ public class Reflect {
         return false;
     }
 
+
+    public static int compare(Object o1, Object o2){
+        if (o1 instanceof String && o2 instanceof String) {
+            return ((String)o1).compareTo(((String)o2)) ;
+        }
+        else if (o1 instanceof Number && o2 instanceof Number) {
+            return Double.compare(Double.valueOf(o1.toString()), Double.valueOf(o2.toString()));
+        }
+        else 
+        {
+            return 0 ; // ni un nombre ni un string
+        }
+    }
+
+    /*
+     * recursion pour eviter de faire une clone si iteration
+     */
+    public static Object getValue(Object o , String [] fieldName, int beginLoop) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+    {
+
+        Field o1Field = o.getClass().getDeclaredField(fieldName[beginLoop]);
+        o1Field.setAccessible(true);
+
+        if (beginLoop!=fieldName.length-1) 
+        {
+            return getValue(o1Field.get(o), fieldName, beginLoop+1) ;
+        }
+
+        return o1Field.get(o);
+
+    }
+
 }
