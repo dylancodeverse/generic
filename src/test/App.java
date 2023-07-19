ccf293894e64d45c70426d53e542737549c4bd2a
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-07-19 16:08:52 +0300

    ok Test

--- src/test/App.java
+++ src/test/App.java
@@ -2,7 +2,9 @@ package test;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println(new Person().getFormHTML("traitement", "get"));
+        System.out.println(new Person().getFormHTMLSpecified("traitement", "get" , 
+        new String[] { "Nom","Age"  } ,"dateDeNaissance" ));
+
 
     }
 
