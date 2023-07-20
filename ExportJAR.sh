228b4c11a455ddbde86e29c0110c892a96c0c6a5
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-07-20 23:13:32 +0300

    exporting generic to a jar file

--- ExportJAR.sh
+++ ExportJAR.sh
@@ -1,2 +1,8 @@
 # compiling source code
 javac  -d ./bin --enable-preview --release 19 $(find  ./src -name '*.java') 
+
+# exporting generic to a jar file
+
+cd ./bin
+
+jar cvf ../everest-spring.jar *
