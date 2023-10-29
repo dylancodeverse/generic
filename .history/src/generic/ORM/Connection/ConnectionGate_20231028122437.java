16b4c2d7e23a8743fb13c865bd2a5882f2ad983d
DylanEverest <ratianarivodylan@gmail.com>
2023-10-29 11:20:36 +0300

    new

---
+++ .history/src/generic/ORM/Connection/ConnectionGate_20231028122437.java
@@ -0,0 +1,43 @@
+package generic.ORM.Connection;
+
+public class ConnectionGate {
+    protected String url;
+    protected String user;
+    protected String password;
+
+    public ConnectionGate() {
+    }
+
+    public ConnectionGate(String url, String user, String password) 
+    {
+        setUrl(url);
+        setUser(user)
+    }
+
+
+    // GETTERS AND SETTERS
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ .history/src/generic/ORM/Connection/GeneralConnection_20231028123049.java
@@ -0,0 +1,63 @@
+package generic.ORM.Connection;
+
+import java.sql.Connection;
+
+public abstract class GeneralConnection {
+
+    protected String url;
+    protected String user;
+    protected String password;
+    protected boolean useAutoCommit;
+
+
+    public GeneralConnection() {
+    }
+
+    public GeneralConnection(String url, String user, String password) {
+
+        setUrl(url);
+        setUser(user);
+        setPassword(password);
+
+    }
+
+    public abstract Connection connect();
+
+    public Connection connect(boolean useAutoCommit){
+        setA   
+    }
+
+    // GETTERS AND SETTERS
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+        public boolean isUseAutoCommit() {
+        return useAutoCommit;
+    }
+
+    public void setUseAutoCommit(boolean useAutoCommit) {
+        this.useAutoCommit = useAutoCommit;
+    }
+
+
+}
